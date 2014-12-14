09270cfdbf4f7db518426d5045c118183a540053
Evan Meagher <evm@twitter.com>
2014-12-14 13:55:28 -0800

    Add usage example to README and rustdoc.

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,36 @@
 //!
 //! It uses [getopts](http://doc.rust-lang.org/getopts/) for argument parsing
 //! and thus currently inherits `getopt`'s `experimental` stability level.
+//!
+//! ## Usage
+//!
+//! The following example shows how to integrate `cli` functions into a program's
+//! `main` function in order to avoid `getopts` boilerplate. Sane defaults are
+//! provided for help and version output without forcing consuming code to buy into
+//! an application framework.
+//!
+//!     extern crate cli;
+//!     extern crate getopts;
+//!
+//!     fn main() {
+//!         let opts = &[
+//!             cli::helpopt(),
+//!             cli::versionopt(),
+//!             getopts::optopt("o", "", "Set output file name", "FILENAME"),
+//!         ];
+//!
+//!         let matches = cli::parse_args(opts);
+//!         if matches.opt_present("h") {
+//!             println!("{}", cli::usage_string(opts));
+//!             return;
+//!         }
+//!         if matches.opt_present("version") {
+//!             println!("{}", cli::version_string("0.0.1"));
+//!             return;
+//!         }
+//!
+//!         ...
+//!     }
 
 #![crate_name = "cli"]
 #![experimental]
