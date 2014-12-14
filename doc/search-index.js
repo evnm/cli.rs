cb30e0c7431c168502d4fa9b94dcefd3d05cc970
Evan Meagher <evm@twitter.com>
2014-12-14 13:42:16 -0800

    rustdoc push by evm

---
+++ doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['cli'] = {"items":[[0,"","cli","A toolkit for building command-line programs in Rust."],[3,"argv0","","The name by which the executed program was called."],[3,"usage_string","","Construct a canonical usage string from a collection of `OptGroup`s."],[3,"version_string","","Construct a version string."],[3,"parse_args","","Parse the command-line arguments with which the program was executed\naccording to a collection of `OptGroup`s."],[3,"helpopt","","Create a help flag `OptGroup`."],[3,"versionopt","","Create a version flag `OptGroup`."]],"paths":[]};
+initSearch(searchIndex);
