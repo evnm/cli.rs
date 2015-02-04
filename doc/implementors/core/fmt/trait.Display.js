8b7a140c27504b9ccca8e913a83925e82677be13
Evan Meagher <evan.meagher@gmail.com>
2015-02-04 15:34:36 -0800

    rustdoc push by evanm

---
+++ doc/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['getopts'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='getopts/enum.Fail.html' title='getopts::Fail'>Fail</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
